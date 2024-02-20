fe95488eb1b8ee20921afd2137fc82f3f73d2680
Mikael Brockman <mikael@brockman.se>
2024-02-20 02:49:08 +0200

    fix some bugs

--- talk-mode.el
+++ talk-mode.el
@@ -69,6 +69,9 @@
 	   :stderr (generate-new-buffer-name "*minigram-error*")
 	   :command `("minigram" 
 		      "--diarize=true"
+		      "--smart-format=true"
+		      "--interim-results=true"
+		      "--diarize=true"
 		      ,(concat "--language=" language))
 	   :noquery t
 	   :connection-type nil
